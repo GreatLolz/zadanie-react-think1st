388616913cfa1878e56cf82d07a7966a936a1dfa
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-08-28 16:35:37 +0200

    update: change input border to violet

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ export default function Input({ label, value, type, onChange }: InputProps) {
     return (
         <div className="flex flex-col w-full gap-1">
             <label className="text-sm">{label}</label>
-            <input className="w-full bg-white border-1 border-neutral-300 rounded-lg px-4 py-2 focus:outline-2 focus:outline-violet-800" 
+            <input className="w-full bg-white border-1 border-violet-300 rounded-lg px-4 py-2 focus:outline-2 focus:outline-violet-800" 
                 type={type} 
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
