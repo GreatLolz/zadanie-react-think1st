c78e0611d7851b82a6ce3decebd4672fda634932
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-08-29 13:43:53 +0200

    refactor: move svgs to icon components

--- src/components/DateInput/DateInput.tsx
+++ src/components/DateInput/DateInput.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import "./dateInput.css"
 import { WORKOUT_HOURS, type Holiday } from "../../types/form";
 import { getHolidayData } from "../../utils/api";
+import InfoIcon from "../icons/InfoIcon";
 
 function isSameDay(d1: Date, d2: Date): boolean {
     return (
@@ -95,8 +96,8 @@ export default function DateInput({ label, onChange }: DateInputProps) {
                 />
                 { info && 
                     <div className="flex flex-row text-sm leading-4 gap-2 mt-2">
-                        <span className="">
-                        <svg width="15" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#b39ddb"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>information-circle-solid</title> <g id="Layer_2" data-name="Layer 2"> <g id="invisible_box" data-name="invisible box"> <rect width="48" height="48" fill="none"></rect> </g> <g id="icons_Q2" data-name="icons Q2"> <path d="M24,2A22,22,0,1,0,46,24,21.9,21.9,0,0,0,24,2Zm2,32a2,2,0,0,1-4,0V22a2,2,0,0,1,4,0ZM24,16a2,2,0,1,1,2-2A2,2,0,0,1,24,16Z"></path> </g> </g> </g></svg>
+                        <span>
+                            <InfoIcon />
                         </span>
                         It is {info}
                     </div>
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,3 +1,4 @@
+import ErrorIcon from "../icons/ErrorIcon";
 import type { InputProps } from "./types";
 
 export default function Input({ label, value, type, onChange, error }: InputProps) {
@@ -16,7 +17,7 @@ export default function Input({ label, value, type, onChange, error }: InputProp
                 error && 
                 <div className="flex flex-row text-xs leading-4 w-50 gap-2">
                     <span className="mt-1">
-                        <svg width="15" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" fill="#ef5350"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"></path> </g></svg>
+                        <ErrorIcon />
                     </span>
                     {error}
                 </div>
