a493f746dfa3aef7a415ac91e653b48351651380
greatlolz <88207459+GreatLolz@users.noreply.github.com>
2025-08-29 13:18:47 +0200

    refactor: conditional render of time picker

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -102,21 +102,21 @@ export default function DateInput({ label, onChange }: DateInputProps) {
                     </div>
                 }
             </div>
-            
-            <div className="flex flex-col gap-1">
-                {date ? <label className="text-sm">Time</label> : <></> }
-                <div className="flex flex-row gap-2 flex-wrap">
-                    {date ? WORKOUT_HOURS.map((hour, index) => {
-                        return (
-                            <label key={index} className={`hover:cursor-pointer bg-white p-2 px-3 rounded-md ${workoutHour === hour ? "border-violet-700 border-2" : "border-violet-300 border-1"}`}>
-                                <input type="radio" name="workoutHour" value={hour} checked={workoutHour === hour} onChange={() => {handleHourChange(hour)}} className="hidden"/>
-                                {hour}
-                            </label>
-                        )
-                    }) : <></>}
+            {date && 
+                <div className="flex flex-col gap-1">
+                    <label className="text-sm">Time</label>
+                    <div className="flex flex-row gap-2 flex-wrap">
+                            {WORKOUT_HOURS.map((hour, index) => {
+                                return (
+                                    <label key={index} className={`hover:cursor-pointer bg-white p-2 px-3 rounded-md ${workoutHour === hour ? "border-violet-700 border-2" : "border-violet-300 border-1"}`}>
+                                        <input type="radio" name="workoutHour" value={hour} checked={workoutHour === hour} onChange={() => {handleHourChange(hour)}} className="hidden"/>
+                                        {hour}
+                                    </label>
+                                )
+                            })}
+                    </div>
                 </div>
-                
-            </div>
+            }
         </div>
     )
 }
